b2d5b5717177e4c546b5f241457770d6564ad27f
Matej Talirevic <matej@bloola.com>
2022-11-29 13:28:38 +0100

    Modified breakpoints based on design

--- src/theme/breakpoints.ts
+++ src/theme/breakpoints.ts
@@ -3,9 +3,9 @@ import {BreakpointsOptions} from '@mui/material'
 const breakpoints: BreakpointsOptions = {
     values: {
         xs: 0,
-        sm: 600,
-        md: 960,
-        lg: 1280,
+        sm: 420,
+        md: 768,
+        lg: 1220,
         xl: 1440,
     },
 }
