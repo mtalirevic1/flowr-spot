df6a8cbfb1857470af619f906045bbf7a8ad974b
Matej Talirevic <matej@bloola.com>
2022-12-01 02:56:22 +0100

    added user property to authSlice state

--- src/redux/slices/authSlice.ts
+++ src/redux/slices/authSlice.ts
@@ -1,27 +1,32 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit'
+import {User} from '../../model/user'
 
 type AuthState = {
     token: string
     isLoggedIn: boolean
+    user: undefined | User
 }
 
 const initAuthState: AuthState = {
     token: '',
-    isLoggedIn: false
+    isLoggedIn: false,
+    user: undefined
 }
 
 export const authState = createSlice({
     name: 'auth',
     initialState: initAuthState,
     reducers: {
-        login: (state, action: PayloadAction<{ token: string }>) => {
-            const {token} = action.payload
+        login: (state, action: PayloadAction<{ token: string, user: User }>) => {
+            const {token, user} = action.payload
             state.token = token
             state.isLoggedIn = true
+            state.user = user
         },
         logout: (state) =>{
             state.token = ''
             state.isLoggedIn = false
+            state.user = undefined
         }
     }
 })
