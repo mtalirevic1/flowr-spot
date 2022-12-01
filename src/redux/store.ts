fa89d9ed93a7157644fe68593eec927766c3f810
Matej Talirevic <matej@bloola.com>
2022-12-01 16:02:57 +0100

    redux store updated

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
 
 import authSlice from './slices/authSlice';
+import uiSlice from "./slices/uiSlice";
 
 export const store = configureStore({
     reducer: {
-        auth: authSlice
+        auth: authSlice,
+        ui: uiSlice
     }
 });
 
