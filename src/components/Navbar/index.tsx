e90e1507bf902e477b21e5d3bfe0ef69ae457167
Matej Talirevic <matej@bloola.com>
2022-12-02 15:18:27 +0100

    Added linking to homepage on logo and title

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,14 @@
-import {AppBar, Stack, Toolbar, Typography, useMediaQuery, useTheme} from "@mui/material"
+import {AppBar, Button, ButtonBase, Link, Stack, Toolbar, useMediaQuery, useTheme} from "@mui/material"
 import Logo from '../../assets/logo.svg'
 import NavMenu from './NavMenu'
 import {NAVBAR_HEIGHT} from '../../constants/layout'
-import {useSelector} from "react-redux";
-import {RootState} from "../../redux/store";
+import {useSelector} from "react-redux"
+import {RootState} from "../../redux/store"
+import {useNavigate} from 'react-router-dom'
 
 const Navbar = () => {
     const theme = useTheme()
+    const navigate = useNavigate()
     const isNotDesktop = useMediaQuery(theme.breakpoints.between('xs', 'md'))
     const {isNavDrawerOpen} = useSelector((state: RootState) => state.ui)
     return (
@@ -20,10 +22,26 @@ const Navbar = () => {
             }}>
             <Toolbar variant="dense" sx={{height: NAVBAR_HEIGHT, m: '0 8px 0 8px'}}>
                 <Stack direction="row" spacing={3} flexGrow={1} alignItems="center">
-                    <img alt="logo" src={Logo} style={{userSelect: "none"}}/>
-                    <Typography fontSize="20px" fontWeight={700} fontFamily="Montserrat" color="primary">
+                    <ButtonBase
+                        onClick={() => navigate('/')}
+                        disableRipple
+                        sx={{'&:hover': {background: 0}}}
+                    >
+                        <img alt="logo" src={Logo} style={{userSelect: "none"}}/>
+                    </ButtonBase>
+                    <Link
+                        variant="h3"
+                        fontWeight={700}
+                        fontFamily="Montserrat"
+                        color="primary"
+                        underline="none"
+                        component={Button}
+                        disableRipple
+                        sx={{'&:hover': {background: 0}}}
+                        onClick={() => navigate('/')}
+                    >
                         FlowrSpot
-                    </Typography>
+                    </Link>
                 </Stack>
                 <NavMenu/>
             </Toolbar>
