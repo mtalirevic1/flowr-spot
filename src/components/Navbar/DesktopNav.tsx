bcd4c9820509e3abe00d392d7b871164f50b94ab
Matej Talirevic <matej@bloola.com>
2022-12-02 01:03:34 +0100

    added avatar placeholder, modified navbar elements style

--- src/components/Navbar/DesktopNav.tsx
+++ src/components/Navbar/DesktopNav.tsx
@@ -1,9 +1,10 @@
-import {Button, Link, Stack} from "@mui/material";
+import {Avatar, Button, Link, Stack} from "@mui/material";
 import {PrimaryButton} from "./Style";
 import {setIsNavDrawerOpen} from "../../redux/slices/uiSlice";
 import {useNavigate} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../redux/store";
+import AvatarImage from "../../assets/avatarPic.png";
 
 const DesktopNav = () => {
     const navigate = useNavigate()
@@ -28,12 +29,11 @@ const DesktopNav = () => {
     }
 
     return (
-        <Stack direction="row" spacing={10} alignItems="center">
+        <Stack direction="row" spacing={15} alignItems="center">
             <Link
                 component={Button}
                 underline='none'
                 variant='h5'
-                fontFamily="Montserrat"
                 sx={{color: (theme) => theme.palette.neutral.main}}
                 onClick={() => handleNavClick('/flowers')}
             >
@@ -43,19 +43,46 @@ const DesktopNav = () => {
                 component={Button}
                 underline='none'
                 variant='h5'
-                fontFamily="Montserrat"
                 sx={{color: (theme) => theme.palette.neutral.main}}
                 onClick={() => handleNavClick('/sightings')}
             >
                 Latest Sightings
             </Link>
+            {isLoggedIn && user &&
+                <>
+                    <Link
+                        component={Button}
+                        underline='none'
+                        variant='h5'
+                        sx={{color: (theme) => theme.palette.neutral.main}}
+                        onClick={() => handleNavClick('/favorites')}
+                    >
+                        Favorites
+                    </Link>
+                <Stack direction='row' alignItems='center'>
+                    <Link
+                        component={Button}
+                        underline='none'
+                        variant='h5'
+                        sx={{color: (theme) => theme.palette.neutral.main}}
+                        onClick={handleUserClick}
+                    >
+                        {`${user.first_name} ${user.last_name}`}
+                    </Link>
+                    <Avatar
+                        alt={`${user.first_name} ${user.last_name}`}
+                        src={AvatarImage}
+                        sx={{width: '40px', height: '40px', ml: '20px'}}
+                    />
+                </Stack>
+                </>
+            }
             {!isLoggedIn &&
                 <div>
                     <Link
                         component={Button}
                         underline='none'
                         variant='h5'
-                        fontFamily="Montserrat"
                         onClick={handleLoginClick}
                         mr='22px'
                     >
