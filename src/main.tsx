2860f843b97bfe7c28d2adf68c311f22dac0a469
Matej Talirevic <matej@bloola.com>
2022-12-04 23:44:59 +0100

    added localization provider for dates

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import {store} from './redux/store'
-import {Provider} from "react-redux";
+import {Provider} from "react-redux"
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider'
+import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App/>
-        </Provider>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </LocalizationProvider>
     </React.StrictMode>
 )
