33da7e36687e8440338148c377c01949dcf540b9
Matej Talirevic <matej@bloola.com>
2022-12-07 00:01:50 +0100

    methods for fetching flowers

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,12 +1,28 @@
 import axios from 'axios'
 import {User, UserRegistration} from '../model/user'
 import {AuthResponse} from "../model/authResponse"
+import {Flower} from '../model/flower'
 
 axios.defaults.baseURL = "https://flowrspot-api.herokuapp.com/api/v1"
 axios.defaults.headers.get['Accept'] = 'application/json'
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 
 const axiosInstance = axios.create()
+
+export const getFlowersRequest = async (search: string): Promise<Flower[]> => {
+    try{
+        const apiPath = search? `/flowers/search?query=${search}` : '/flowers'
+        const res = await axiosInstance.get(apiPath)
+        return res.data.flowers
+    } catch (e) {
+        const errorString = 'Unable to load flowers'
+        if (axios.isAxiosError(e)) {
+            throw new Error(e.response ? e.response.data.error : errorString)
+        } else {
+            throw new Error(errorString)
+        }
+    }
+}
 
 export const registerUserRequest = async (user: UserRegistration): Promise<string> => {
     try {
