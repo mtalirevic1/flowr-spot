471468951f03fd6edf4500bfc041069d0cdc2282
Matej Talirevic <matej@bloola.com>
2022-12-06 02:07:29 +0100

    searchbar implemented

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,7 +1,8 @@
-import {ColumnBox} from '../../GlobalStyle'
+import {ColumnBox, IconButtonNoBg} from '../../GlobalStyle'
 import {NAVBAR_HEIGHT} from '../../constants/layout'
 import HeroImage from '../../assets/heroImage.png'
-import {Typography} from '@mui/material'
+import {InputBase, Typography} from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search'
 
 
 const Home = () => {
@@ -13,31 +14,52 @@ const Home = () => {
             }}
         >
             <div
-            style={{
-                height: '500px',
-                width: '100%',
-                background: `url(${HeroImage})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                flexDirection: 'column',
-                textAlign: 'center'
-            }}
+                style={{
+                    height: '500px',
+                    width: '100%',
+                    background: `url(${HeroImage})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flexDirection: 'column',
+                    textAlign: 'center'
+                }}
             >
-                <Typography variant='h1' color='white' sx={{mb: '15px'}}>
+                <Typography variant="h1" color="white" sx={{mb: '15px'}}>
                     Discover flowers around you
                 </Typography>
                 <Typography
-                    fontSize='17px'
+                    fontSize="17px"
                     fontWeight={300}
-                    color='white'
+                    color="white"
                     sx={{opacity: 0.7, mb: '45px'}}
                 >
                     Explore between more than 8.427 sightings
                 </Typography>
+                <InputBase
+                    sx={{
+                        background: (theme) => theme.palette.common.white,
+                        borderRadius: '3px',
+                        p: '0 20px 0 20px',
+                        width: {
+                            md: '600px',
+                            sm: '450px',
+                            xs: '300px'
+                        },
+                        height: '56px',
+                        fontWeight: 300,
+                        fontSize: '18px'
+                    }}
+                    placeholder="Looking for something specific?"
+                    endAdornment={
+                        <IconButtonNoBg>
+                            <SearchIcon fontSize='large' color="primary"/>
+                        </IconButtonNoBg>
+                    }
+                />
             </div>
         </ColumnBox>
     )
